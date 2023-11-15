e8c786c67ba42891bd0d16b71f1da906106a75c4
Rosi-07 <116937130+Rosi-07@users.noreply.github.com>
2023-11-15 14:35:46 -0600

    Add Unidad to Alimento Model

---
+++ Alimento.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccess.Model;
+
+public partial class Alimento
+{
+    public string IdAlimento { get; set; } = null!;
+
+    public string NombreAlimento { get; set; } = null!;
+
+    public DateTime FechaVencimiento { get; set; }
+
+    public double Peso { get; set; }
+
+    public string Unidad { get; set; }
+
+    public virtual ICollection<EnvioAlimento> EnvioAlimentos { get; set; } = new List<EnvioAlimento>();
+
+    public virtual ICollection<InventarioAlimento> InventarioAlimentos { get; set; } = new List<InventarioAlimento>();
+}
